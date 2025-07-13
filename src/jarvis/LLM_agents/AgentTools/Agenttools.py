678344671777431aa9965ce967ee264f60cb78c1
DanYoto <50916221@qq.com>
2025-07-13 11:15:00 +0200

    add googlemap api to find geographical information

--- src/jarvis/LLM_agents/AgentTools/Agenttools.py
+++ src/jarvis/LLM_agents/AgentTools/Agenttools.py
@@ -44,3 +44,45 @@ def get_current_time(location: Optional[str] = None) -> str:
     return (
         f"Current local time is {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}"
     )
+
+@tool
+def use_google_map(position_1: str, position_2: str, mode: str, transit_mode: str = None) -> str:
+    """
+    Use Google Maps to find the distance between two places and estimate the time it takes to travel between two places.
+
+    Args:
+    position_1: Name of starting position.
+    position_2: Name of destination position.
+    mode: Transportation mode - "driving", "walking", "bicycling", or "transit".
+    transit_mode: Optional. Only used when mode is 'transit'
+                Optional: 'bus', 'subway', 'train', 'tram', 'rail', or combinations like 'bus,subway'
+    """
+    import googlemaps
+    import os
+    from datetime import datetime  
+    gmaps = googlemaps.Client(key=os.environ["google_maps_api_key"])
+    params = {
+        "origins": position_1,
+        "destinations": position_2,
+        "mode": mode,
+        "departure_time": datetime.now(),
+    }
+    if mode == "transit":
+        departure_time = datetime.now()
+        if transit_mode:
+             if isinstance(transit_mode, str):
+                transit_modes = [m.strip() for m in transit_mode.split(",")]
+            else:
+                transit_modes = [transit_mode]
+            
+            valid_modes = ['bus', 'subway', 'train', 'tram', 'rail']
+            transit_modes = [m for m in transit_modes if m in valid_modes]
+
+            params['departure_time'] = departure_time
+            params['transit_mode'] = transit_modes
+    elif mode == 'driving':
+        params['departure_time'] = departure_time
+    distance_result = gmaps.distance_matrix(**params)
+    distance = distance_result['rows'][0]['elements'][0]['distance']['text']
+    duration = distance_result['row'][0]['elements'][0]['duration']['text']
+    return f"Distance from {position_1} to {position_2} is {distance} and it takes approximately {duration} to travel by {mode}."
